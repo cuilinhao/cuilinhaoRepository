d5cbd5d757b0d0ac07b861b49d51ec35c9657075
林豪 <linhao@apple9deiMac.local>
2015-07-01 11:13:14 +0800

    Initial Commit

---
+++ ee/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ee
+//
+//  Created by 林豪 on 15/7/1.
+//  Copyright (c) 2015年 林豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ee/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ee
+//
+//  Created by 林豪 on 15/7/1.
+//  Copyright (c) 2015年 林豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
